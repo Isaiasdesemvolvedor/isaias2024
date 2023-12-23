e7ef91b97b5cd01d497a506f0653f031b08d2cfc
isaiasdesemvolvedor <isaiasdesemvolvedor>
2023-12-23 14:29:42 -0300

    landing page

---
+++ landing page fastfood.js
@@ -0,0 +1,7 @@
+function lateral(){ 
+const lateral = document.getElementById("list");
+lateral.style.width="25%";
+}
+function fechar(){ 
+    const lateral2 = document.getElementById("list");
+    lateral2.style.width="0px";}
